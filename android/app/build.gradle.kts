c66f5c0ec3cc8201e7db7df5efbe56cf84832433
hrganeshta-cmd <hrganeshta@gmail.com>
2026-06-16 10:48:22 +0530

    Add files via upload

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "in.ganpatrai.helpdesk"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "in.ganpatrai.helpdesk"
+        minSdk = 24          // Android 7.0 and above
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+}
